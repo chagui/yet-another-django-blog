4ca6bd810a488b4c7e20aef1175407ed4319375a
Guilhem C <guilhem.charles@gmail.com>
2019-09-21 15:58:22 +0200

    Add comments to blog post

--- website/blog/models.py
+++ website/blog/models.py
@@ -7,5 +7,19 @@ class Post(models.Model):
     pub_date = models.DateTimeField('date published')
     content = models.TextField()
 
+    def get_comments(self):
+        return Comment.objects.filter(post=self)
+
+    def number_of_comments(self):
+        return self.get_comments().count()
+
     def __str__(self):
         return f'"{self.title}" by {self.author} - {self.pub_date.strftime("%B %d, %Y")}'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    pub_date = models.DateTimeField('date published')
+    content = models.TextField()
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
 
-from .models import Post
+from .forms import CommentForm
+from .models import Comment, Post
 
 
 class IndexView(generic.ListView):
@@ -12,6 +18,26 @@ class IndexView(generic.ListView):
         return Post.objects.order_by('-pub_date')
 
 
-class PostView(generic.DetailView):
+class PostView(generic.UpdateView):
     model = Post
     template_name = 'blog/post.html'
+    form_class = CommentForm
+
+    def get_context_data(self, **kwargs):
+        context = super(PostView, self).get_context_data(**kwargs)
+        context['comments'] = self.get_object().get_comments()
+        context['form'] = CommentForm
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        post = self.get_object()
+        if form.is_valid():
+            Comment.objects.create(**form.cleaned_data, post=post, pub_date=datetime.now())
+            return HttpResponseRedirect(reverse('blog:post', kwargs={'pk': post.id}))
+        context = {
+            'post': post,
+            'comments': post.get_comments(),
+            'form': form
+        }
+        return render(request, self.template_name, context)
